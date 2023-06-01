185439923ce95f8518de400b7eb0465a100a69a2
Pragya Pandey <86335220+Pragya-Pandey2709@users.noreply.github.com>
2023-06-01 20:33:49 +0530

    Update churn _pred.py

--- ANN/churn _pred.py
+++ ANN/churn _pred.py
@@ -92,7 +92,7 @@ y_pred = (y_pred > 0.5)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-# Calculate the Accuracy
+
 from sklearn.metrics import accuracy_score
 score=accuracy_score(y_pred,y_test)
 
